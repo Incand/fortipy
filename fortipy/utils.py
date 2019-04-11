295014635a1ded8dbc184e7f8dc2faae37854e49
Armin Test <armin.schaare@crocodial.de>
2019-04-11 15:12:55 +0200

    Removed unnecessary replacement of underline with spaces

--- fortipy/utils.py
+++ fortipy/utils.py
@@ -11,4 +11,4 @@ def kwargs_to_json_handler(kwargs):
     Necessary for python args and API params name conflict resolution.
     e.g. for table "get_used" -> "get used" or "filter_" -> "filter".
     '''
-    return {k.strip('_').replace('_', ' '): v for k, v in kwargs.items()}
+    return {k.strip('_'): v for k, v in kwargs.items()}
